8c0f15a3d50a5a9b7266819a6ab77a34e128fb08
Saramanda9988 <2074730050@qq.com>
2026-06-13 16:30:50 +0800

    chore: avoid ipv4 mismatch in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), UnoCSS()],
   server: {
+    host: "127.0.0.1",
     proxy: {
       "/proxy/image": {
         target: "http://127.0.0.1:23680",
